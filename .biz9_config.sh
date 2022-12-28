b31749af8526b19011140f3a18644fd939ef7ea4
tank <tank@certifiedcoderz.com>
2022-12-27 20:46:54 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
 BIZ9_WEBSITE_VERSION='3.7.3';
-APP_VERSION='1.8.3'
+APP_VERSION='1.8.4'
 APP_ID="19";
 APP_TITLE="BiZ9-Web";
 APP_TITLE_ID='website-19';
@@ -7,4 +7,6 @@ EC2_IP_ADDR="0.00.000.000"
 EC2_DEPLOY_DIR="/home/admin/www/website-_app_id_"
 EC2_KEY_FILE="other/aws/ec2_key/_app_id_.pem"
 REPO_URL="git@github.com:bossappz/biz9-website-stable.git";
+MONGO_PORT='27019'
+
 
