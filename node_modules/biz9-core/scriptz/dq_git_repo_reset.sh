4401147d1fcb542dfafcafc6eab24d0331edcf75
tank <tank@certifiedcoderz.com>
2022-12-28 00:38:46 -0500

    good

--- node_modules/biz9-core/scriptz/dq_git_repo_reset.sh
+++
@@ -1,9 +0,0 @@
-source ./.biz9_config.sh
-echo "#################"
-echo "BiZ9 Framework Git Repo Reset"
-echo "#################"
-bash ./scriptz/dq_header.sh
-git reset -- hard
-git reset --hard ${BRANCH}
-bash ./scriptz/dq_footer.sh
-exit
